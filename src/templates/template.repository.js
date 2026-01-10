d11e17f3420988fb3ff14c5f3246245f32fec30b
gerson <gersonmoliver@gmail.com>
2026-01-10 03:33:05 -0800

    implemented sp call

--- src/templates/template.repository.js
+++ src/templates/template.repository.js
@@ -13,41 +13,8 @@ exports.findAll = async () => {
 
 exports.findById = async (id) => {
   const sql = `
-    SELECT
-      t.id,
-      t.templateName,
-      (
-        SELECT jsonb_agg(groups_agg)
-        FROM (
-          SELECT
-            g.id,
-            g.name,
-            g.position,
-            (
-              SELECT jsonb_agg(fields_agg ORDER BY f.position)
-              FROM (
-                SELECT
-                  f.id,
-                  f.key,
-                  f.label,
-                  f.type,
-                  f.required,
-                  f.position,
-                  f.options,
-                  f.settings
-                FROM rentals_db.fields f
-                WHERE f.groupId = g.id
-              ) fields_agg
-            ) AS fields
-          FROM rentals_db.groups g
-          WHERE g.templateId = t.id
-          ORDER BY g.position
-        ) groups_agg
-      ) AS groups
-    FROM rentals_db.templates t
-    WHERE t.id = 'a1b2c3d4-e5f6-7890-1234-567890abcdef'
+    SELECT rentals_db.get_template_by_id($1) AS template
   `;
-
   const { rows } = await db.query(sql, [id]);
-  return rows[0];
+  return rows[0]?.template || null;
 };
