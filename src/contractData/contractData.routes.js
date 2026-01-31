1bb5ed05de132f8c2afd95e706cee611fc5126dd
gerson <gersonmoliver@gmail.com>
2026-01-31 13:07:30 -0800

    refactor: deleted files

--- src/contractData/contractData.routes.js
+++
@@ -1,8 +0,0 @@
-// src/contractData/contractData.routes.js
-const express = require("express");
-const router = express.Router();
-const controller = require("./contractData.controller");
-
-router.post("/", controller.saveContractData);
-
-module.exports = router;
